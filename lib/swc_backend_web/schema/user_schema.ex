aaf10ca60559b642e20b5bd9b163f5d68a8ef343
NtwaliHeritier <heritierntwali@gmail.com>
2021-08-23 23:31:24 +0200

    add new user fields to schema

--- lib/swc_backend_web/schema/user_schema.ex
+++ lib/swc_backend_web/schema/user_schema.ex
@@ -3,14 +3,26 @@ defmodule SwcBackendWeb.Schema.Types.UserSchema do
 
     object :user_type do
         field :id, :id
+        field :names, :string
+        field :age, :integer
+        field :gender, :string
         field :email, :string
         field :username, :string
+        field :phone, :string
+        field :picture, :string
+        field :status, :string
     end
 
     input_object :user_input_type do
+        field :names, non_null(:string)
+        field :age,  non_null(:integer)
+        field :gender, non_null(:string)
         field :email, non_null(:string)
         field :username, non_null(:string)
         field :password, non_null(:string)
         field :password_confirmation, non_null(:string)
+        field :phone,  non_null(:string)
+        field :picture,  non_null(:string)
+        field :status,  non_null(:string)
     end
 end
