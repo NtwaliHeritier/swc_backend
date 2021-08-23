f4c0dc1bac98fd61c34204c543bceeb4dceac081
NtwaliHeritier <heritierntwali@gmail.com>
2021-08-23 20:23:06 +0200

    add refactoring to post save feature

--- lib/swc_backend_web/resolvers/post_resolvers.ex
+++ lib/swc_backend_web/resolvers/post_resolvers.ex
@@ -6,52 +6,75 @@ defmodule SwcBackendWeb.Resolvers.PostResolvers do
     end
 
     def create_post(_,%{input: %{picture: _picture, video: _video} = input}, %{context: %{current_user: user}}) do
-        input = Map.merge(input, %{user_id: user.id})
+        input = compute_input(input, user)
         video_pid = Task.async(fn -> Cloudex.upload(input[:video], %{resource_type: "video"}) end)
         picture_pid = Task.async(fn -> Cloudex.upload(input[:picture]) end)
         
-        with {:ok, %{url: video_url}} <- Task.await(video_pid),
-             {:ok, %{url: picture_url}} <- Task.await(picture_pid),
-             {:ok, post} <- Articles.create_post(%{input | picture: picture_url, video: video_url})
-        do
-            {:ok, post}
-        else
-            _ ->
-                {:error, message: "Post not created, make sure all fields are filled correctly"}
-        end
+        {:ok, %{url: video_url}} = Task.await(video_pid)
+        {:ok, %{url: picture_url}} = Task.await(picture_pid)
+
+        # with {:ok, %{url: video_url}} <- Task.await(video_pid),
+        #      {:ok, %{url: picture_url}} <- Task.await(picture_pid),
+        #      {:ok, post} <- Articles.create_post(%{input | picture: picture_url, video: video_url})
+        # do
+        #     {:ok, post}
+        # else
+        #     _ ->
+        #         {:error, message: "Post not created, make sure all fields are filled correctly"}
+        # end
+        compute_create(%{input | picture: picture_url, video: video_url})
     end
 
     def create_post(_,%{input: %{picture: _picture} = input}, %{context: %{current_user: user}}) do
-        input = Map.merge(input, %{user_id: user.id})
+        input = compute_input(input, user)
         picture_pid = Task.async(fn -> Cloudex.upload(input[:picture]) end)
         
-        with {:ok, %{url: picture_url}} <- Task.await(picture_pid),
-             {:ok, post} <- Articles.create_post(%{input | picture: picture_url})
-        do
-            {:ok, post}
-        else
-            _ ->
-                {:error, message: "Post not created, make sure all fields are filled correctly"}
-        end
+        {:ok, %{url: picture_url}} = Task.await(picture_pid)
+        compute_create(%{input | picture: picture_url})
+
+        # with {:ok, %{url: picture_url}} <- Task.await(picture_pid),
+        #      {:ok, post} <- Articles.create_post(%{input | picture: picture_url})
+        # do
+        #     {:ok, post}
+        # else
+        #     _ ->
+        #         {:error, message: "Post not created, make sure all fields are filled correctly"}
+        # end
     end
 
     def create_post(_,%{input: %{video: _video} = input}, %{context: %{current_user: user}}) do
-        input = Map.merge(input, %{user_id: user.id})
+        input = compute_input(input, user)
         video_pid = Task.async(fn -> Cloudex.upload(input[:video], %{resource_type: "video"}) end)
         
-        with {:ok, %{url: video_url}} <- Task.await(video_pid),
-             {:ok, post} <- Articles.create_post(%{input | video: video_url})
-        do
-            {:ok, post}
-        else
-            _ ->
-                {:error, message: "Post not created, make sure all fields are filled correctly"}
-        end
+        {:ok, %{url: video_url}} = Task.await(video_pid)
+        compute_create(%{input | video: video_url})
+        # with {:ok, %{url: video_url}} <- Task.await(video_pid),
+        #      {:ok, post} <- Articles.create_post(%{input | video: video_url})
+        # do
+        #     {:ok, post}
+        # else
+        #     _ ->
+        #         {:error, message: "Post not created, make sure all fields are filled correctly"}
+        # end
     end
 
     def create_post(_,%{input: input}, %{context: %{current_user: user}}) do
-        input = Map.merge(input, %{user_id: user.id})
+        input = compute_input(input, user)
+        compute_create(input)
+        # with {:ok, post} <- Articles.create_post(input)
+        # do
+        #     {:ok, post}
+        # else
+        #     _ ->
+        #         {:error, message: "Post not created, make sure all fields are filled correctly"}
+        # end
+    end
 
+    defp compute_input(input, user) do
+        Map.merge(input, %{user_id: user.id})
+    end
+
+    defp compute_create(input) do
         with {:ok, post} <- Articles.create_post(input)
         do
             {:ok, post}
