372d7ff114cba344bc7bed94f8f2f1d8b8d7ae07
NtwaliHeritier <heritierntwali@gmail.com>
2021-08-24 11:24:11 +0200

    add relationship in schema

--- lib/swc_backend/chats/message.ex
+++ lib/swc_backend/chats/message.ex
@@ -1,11 +1,15 @@
 defmodule SwcBackend.Chats.Message do
   use Ecto.Schema
   import Ecto.Changeset
+  alias SwcBackend.Accounts.User
+  alias SwcBackend.Chats.Room
 
   schema "messages" do
     field :text, :string
     field :user_id, :id
     field :room_id, :id
+    belongs_to :user, User
+    belongs_to :room, Room
 
     timestamps()
   end
--- lib/swc_backend/chats/room.ex
+++ lib/swc_backend/chats/room.ex
@@ -1,9 +1,10 @@
 defmodule SwcBackend.Chats.Room do
   use Ecto.Schema
   import Ecto.Changeset
+  alias SwcBackend.Chats.Message
 
   schema "rooms" do
-
+    has_many :messages, Message
     timestamps()
   end
 
